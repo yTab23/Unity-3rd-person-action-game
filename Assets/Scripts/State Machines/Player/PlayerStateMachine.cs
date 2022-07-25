bc8b0e1a790b23e63ecb9dbdd9c755150c63c185
Yuki Tabata <ytabata11@gmail.com>
2022-07-25 22:32:43 +1000

    Player character will no longer float along the surface of the terrain.

--- Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
@@ -10,6 +10,7 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
     [field: SerializeField] public float RotationSmoothValue { get; private set; }
     [field: SerializeField] public Targeter Targeter { get; private set; }
+    [field: SerializeField] public ForceReceiver ForceReceiver { get; private set; }
 
     [field: SerializeField] public Animator Animator { get; private set; }
 
