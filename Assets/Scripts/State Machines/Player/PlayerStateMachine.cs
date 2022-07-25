65b91a0e99af026bc015ae3edb97cb3621a2ab1d
Yuki Tabata <ytabata11@gmail.com>
2022-07-25 16:53:11 +1000

    Renamed the PlayerTestState to PlayerFreeLookState. Refactored code and added smoothing to the camera rotation.

--- Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/State Machines/Player/PlayerStateMachine.cs
@@ -8,6 +8,8 @@ public class PlayerStateMachine : StateMachine
     [field: SerializeField] public InputReader InputReader { get; private set; }
     [field: SerializeField] public CharacterController Controller { get; private set; }
     [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
+    [field: SerializeField] public float RotationSmoothValue { get; private set; }
+
     [field: SerializeField] public Animator Animator { get; private set; }
 
     public Transform MainCameraTransform { get; private set; }
@@ -16,7 +18,7 @@ public class PlayerStateMachine : StateMachine
     {
         MainCameraTransform = Camera.main.transform;
 
-        SwitchState(new PlayerTestState(this));
+        SwitchState(new PlayerFreeLookState(this));
     }
 
 }
