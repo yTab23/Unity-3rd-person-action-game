12bf64a0282f26e60e4ce0099a7d97513185674a
Yuki Tabata <ytabata11@gmail.com>
2022-07-26 17:59:49 +1000

    The player will be pushed/knocked back when the enemy hits the player.

--- Assets/Scripts/Combat/WeaponDamage.cs
+++ Assets/Scripts/Combat/WeaponDamage.cs
@@ -6,7 +6,10 @@ public class WeaponDamage : MonoBehaviour
 {
     [SerializeField] private Collider myCollider;
 
+
     private int damage;
+    private float knockback;
+    
 
     private List<Collider> alreadyCollidedWith = new List<Collider>();
 
@@ -27,10 +30,18 @@ public class WeaponDamage : MonoBehaviour
         {
             health.DealDamage(10);
         }
+
+        if(other.TryGetComponent<ForceReceiver>(out ForceReceiver forceReceiver))
+        {
+            Vector3 direction = (other.transform.position - myCollider.transform.position).normalized ;
+            forceReceiver.AddForce(direction * knockback);
+        }
     }
 
-    public void SetAttack(int damage)
+    public void SetAttack(int damage, float knockback)
     {
         this.damage = damage;
+        this.knockback = knockback;
+        
     }
 }
--- Assets/Scripts/State Machines/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/State Machines/Enemy/EnemyStateMachine.cs
@@ -11,10 +11,11 @@ public class EnemyStateMachine : StateMachine
     [field: SerializeField] public NavMeshAgent Agent { get; private set; }
     [field: SerializeField] public WeaponDamage Weapon { get; private set; }
 
-    [field: SerializeField] public float AttackRange { get; private set; }
-    [field: SerializeField] public int AttackDamage { get; private set; }
     [field: SerializeField] public float MovementSpeed { get; private set; }
     [field: SerializeField] public float PlayerChasingRange { get; private set; }
+    [field: SerializeField] public float AttackRange { get; private set; }
+    [field: SerializeField] public int AttackDamage { get; private set; }
+    [field: SerializeField] public int AttackKnockback { get; private set; }
 
     public GameObject Player { get; private set; }
 
