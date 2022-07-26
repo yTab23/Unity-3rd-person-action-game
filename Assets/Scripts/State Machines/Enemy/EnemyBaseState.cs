caf5b55cf91aeaead425f18339d69b5f8373acca
Yuki Tabata <ytabata11@gmail.com>
2022-07-26 13:34:57 +1000

    Created an enemy character, and its statemachine and base state

---
+++ Assets/Scripts/State Machines/Enemy/EnemyBaseState.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBaseState : State
+{
+    protected EnemyStateMachine stateMachine;
+
+    public EnemyBaseState(EnemyStateMachine stateMachine)
+    {
+        this.stateMachine = stateMachine;
+    }
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Tick(float deltaTime)
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
